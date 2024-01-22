65fb5fbf4cda1abe31144bc4d2ec7c72eb85371c
Kagiso31 <kagisomokou@gmail.com>
2024-01-22 17:11:33 +0200

    Add sticky header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -3,18 +3,38 @@ import artexLogo from "../../assets/shared/arex-logo-light.png";
 import Navbar from "./Navbar";
 import MobileMenu from "./MobileMenu";
 import SearchForm from "./SearchForm";
+import { useEffect, useRef } from "react";
 
 const Header = () => {
-  return (
-    <header className="header">
-      <Link className="header__link" to="/artex">
-        <img className="header__logo" src={artexLogo} alt="Artex Logo" />
-      </Link>
-      <Navbar />
-      <SearchForm />
+  const headerRef = useRef<HTMLElement>(null);
+  const scrollWatcherRef = useRef<HTMLDivElement>(null);
 
-      <MobileMenu />
-    </header>
+  useEffect(() => {
+    const navObserver = new IntersectionObserver((entries) => {
+      headerRef.current?.classList.toggle(
+        "header--sticking",
+        !entries[0].isIntersecting
+      );
+    });
+
+    if (scrollWatcherRef.current !== null) {
+      navObserver.observe(scrollWatcherRef.current);
+    }
+  }, []);
+
+  return (
+    <>
+      <div ref={scrollWatcherRef} data-scroll-watcher />
+      <header ref={headerRef} className="header">
+        <Link className="header__link" to="/artex">
+          <img className="header__logo" src={artexLogo} alt="Artex Logo" />
+        </Link>
+        <Navbar />
+        <SearchForm />
+
+        <MobileMenu />
+      </header>
+    </>
   );
 };
 
