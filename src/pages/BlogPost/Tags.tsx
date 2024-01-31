dfb49d12d17696ec328dadaa406881c56b7b34fd
Kagiso31 <kagisomokou@gmail.com>
2024-01-31 13:37:54 +0200

    Fix tags format

--- src/pages/BlogPost/Tags.tsx
+++ src/pages/BlogPost/Tags.tsx
@@ -9,11 +9,20 @@ const Tags = ({ post }: TagsProps) => {
     <div className="post-tags__container">
       <p className="post-tags__heading">Tags:</p>
       <ul role="list" className="post-tags__list">
-        {post?.tags.map((tag) => (
-          <li key={tag} className="post-tags__item">
-            {tag}
-          </li>
-        ))}
+        {post?.tags.map((tag, index) => {
+          if (index === post.tags.length - 1) {
+            return (
+              <li key={tag} className="post-tags__item">
+                {tag}
+              </li>
+            );
+          }
+          return (
+            <li key={tag} className="post-tags__item">
+              {tag},
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
